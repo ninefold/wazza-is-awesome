0ce345381c5440a44e3353623dc3b097e80776b6
shaundomingo-ninefold <shaun@ninefold.com>
2013-07-02 13:17:13 +1000

    Add some serious database writes

--- app/models/write_to_database_job.rb
+++ app/models/write_to_database_job.rb
@@ -2,6 +2,15 @@ class WriteToDatabaseJob
   @queue = 'test'
 
   def self.perform
-    Post.create! :content => "From a worker", :name => "from-a-worker", :title => "Worker"
+    for i in 1..500
+      puts "Iteration: #{i}/500"
+      for x in 1..10
+        Post.create! :content => "Iteration #{x}|#{i} - From worker",
+          :name => "Node: #{Socket.gethostbyname(Socket.gethostname).first}",
+          :title => "Entry from #{Socket.gethostbyname(Socket.gethostname).first}: #{Socket.ip_address_list.detect{|intf| intf.ipv4_private?}}"
+        sleep rand(3)
+      end
+      sleep rand(5)
+    end
   end
 end
