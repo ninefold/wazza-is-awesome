0d9f97e18db0e24c7f2e015ef20c23c3ee250529
shaundomingo-ninefold <shaun@ninefold.com>
2013-07-02 15:12:38 +1000

    Tune variables a little

--- app/models/write_to_database_job.rb
+++ app/models/write_to_database_job.rb
@@ -3,9 +3,9 @@ class WriteToDatabaseJob
 
   def self.perform
     iterations = 250
-    posts_per_iteration = 1000
-    sleep_between_iterations = 5
-    sleep_between_writes = 0.03
+    posts_per_iteration = 500
+    sleep_between_iterations = 1
+    sleep_between_writes = 0
 
     puts "Creating 1000 records each iteration. #{iterations} iterations. #{sleep_between_iterations} seconds sleep between each iteration. #{sleep_between_writes} second wait between each write."
     for i in 1..250
