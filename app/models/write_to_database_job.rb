337042f13984c93635e268e3919d9d4d8e3896d1
shaundomingo-ninefold <shaun@ninefold.com>
2013-07-02 14:48:17 +1000

    Update job to do a bit more writing, a bit quicker.

--- app/models/write_to_database_job.rb
+++ app/models/write_to_database_job.rb
@@ -2,15 +2,23 @@ class WriteToDatabaseJob
   @queue = 'test'
 
   def self.perform
-    for i in 1..500
-      puts "Iteration: #{i}/500"
-      for x in 1..10
-        Post.create! :content => "Iteration #{x}|#{i} - From worker",
-          :name => "Node: #{Socket.gethostbyname(Socket.gethostname).first}",
-          :title => "Entry from #{Socket.gethostbyname(Socket.gethostname).first}: #{Socket.ip_address_list.detect{|intf| intf.ipv4_private?}}"
-        sleep rand(3)
+    iterations = 250
+    posts_per_iteration = 1000
+    sleep_between_iterations = 5
+    sleep_between_writes = 0.2
+
+    puts "Creating 1000 records each iteration. #{iterations} iterations. #{sleep_between_iterations} seconds sleep between each iteration. #{sleep_between_writes} second wait between each write."
+    for i in 1..250
+      puts "Iteration: #{i}/#{iterations}"
+      for x in 1..posts_per_iteration
+        Post.create! :content => ((0...512).map{ ('a'..'z').to_a[rand(26)] }.join)
+          :name => "Autogen Post, from server: #{Socket.gethostbyname(Socket.gethostname).first}",
+          :title => "New Post at: #{Time.now.inspect}"
+        sleep sleep_between_writes
       end
-      sleep rand(5)
+      sleep rand(sleep_between_iterations)
     end
+    puts "Cleaning up #{Post.count} posts after #{iterations} iterations"
+    Post.delete_all
   end
 end
