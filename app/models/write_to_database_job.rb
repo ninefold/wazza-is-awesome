f388ad6b72a93360af088d7dab535126e5be46b3
Warren Bain <warren@ninefold.com>
2013-10-22 10:36:58 +1100

    Increase b/g job to write about 1GB in 30 mins

--- app/models/write_to_database_job.rb
+++ app/models/write_to_database_job.rb
@@ -2,21 +2,24 @@ class WriteToDatabaseJob
   @queue = 'test'
 
   def self.perform
-    iterations = 1000
-    posts_per_iteration = 500
-    sleep_between_iterations = 1
-    sleep_between_writes = 0
+    # updated to write 1GB of data each job run
+    # which should take about 30 mins to run
+    iterations = 1024
+    posts_per_iteration = 1024
+    post_byte_size = 1024
+    sleep_between_iterations = 2
+    sleep_between_writes = 1
 
     puts "Creating #{posts_per_iteration} records each iteration. #{iterations} iterations. #{sleep_between_iterations} seconds sleep between each iteration. #{sleep_between_writes} second wait between each write."
     for i in 1..iterations
       puts "Iteration: #{i}/#{iterations}"
       for x in 1..posts_per_iteration
-        Post.create! :content => ((0...512).map{ ('a'..'z').to_a[rand(26)] }.join),
+        Post.create! :content => ((0...post_byte_size).map{ ('a'..'z').to_a[rand(26)] }.join),
           :name => "Autogen Post, from server: #{Socket.gethostbyname(Socket.gethostname).first}",
           :title => "New Post at: #{Time.now.inspect}"
-        sleep sleep_between_writes
+        sleep rand * sleep_between_writes
       end
-      sleep rand*sleep_between_iterations
+      sleep rand * sleep_between_iterations
     end
     puts "Created a total of #{Post.count} posts after #{iterations} iterations."
   end
