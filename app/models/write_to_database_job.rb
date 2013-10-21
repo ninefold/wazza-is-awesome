582d8861d71d3134a9bc667f22a646112f812ce1
Warren Bain <warren@ninefold.com>
2013-10-21 14:25:03 +1100

    Refactor resque job to retain posts and lots of them

--- app/models/write_to_database_job.rb
+++ app/models/write_to_database_job.rb
@@ -2,13 +2,13 @@ class WriteToDatabaseJob
   @queue = 'test'
 
   def self.perform
-    iterations = 250
+    iterations = 1000
     posts_per_iteration = 500
     sleep_between_iterations = 1
     sleep_between_writes = 0
 
     puts "Creating #{posts_per_iteration} records each iteration. #{iterations} iterations. #{sleep_between_iterations} seconds sleep between each iteration. #{sleep_between_writes} second wait between each write."
-    for i in 1..250
+    for i in 1..iterations
       puts "Iteration: #{i}/#{iterations}"
       for x in 1..posts_per_iteration
         Post.create! :content => ((0...512).map{ ('a'..'z').to_a[rand(26)] }.join),
@@ -16,9 +16,8 @@ class WriteToDatabaseJob
           :title => "New Post at: #{Time.now.inspect}"
         sleep sleep_between_writes
       end
-      sleep rand(sleep_between_iterations)
+      sleep rand*sleep_between_iterations
     end
-    puts "Cleaning up #{Post.count} posts after #{iterations} iterations"
-    Post.delete_all
+    puts "Created a total of #{Post.count} posts after #{iterations} iterations."
   end
 end
