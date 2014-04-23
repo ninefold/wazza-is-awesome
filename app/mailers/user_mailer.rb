38b7cacbc98d6f13aba9950f2630304aba93ebba
Shaun Domingo <shaun@ninefold.com>
2014-04-23 23:32:41 +1000

    Fix faulty mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,11 @@ class UserMailer < ActionMailer::Base
   def hello_email(post)
     @post = post
     if @post.email
-      mail(to: @post.email, subject: 'Email from wazza-is-awesome')
-      dbg_str = "Sent email to #{@post.email} with env vars: " +
+      dbg_str = "Sending email to #{@post.email} with env vars: " +
         "[domain] #{ENV['NF_SENDGRID_DOMAIN']} [username] #{ENV['NF_SENDGRID_USERNAME']} [password] #{ENV['NF_SENDGRID_PASSWORD']}"
       logger.info dbg_str
       puts dbg_str
+      mail(to: @post.email, subject: 'Email from wazza-is-awesome')
     else
       logger.info "No email specified for this record. Not sending email."
       puts "No email specified for this record. Not sending email."
