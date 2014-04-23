0104b545ae0186284fcc90429ba7342b20d79995
Shaun Domingo <shaun@ninefold.com>
2014-04-23 17:19:35 +1000

    Debug environment variables

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,10 @@ class UserMailer < ActionMailer::Base
     @post = post
     if @post.email
       mail(to: @post.email, subject: 'Email from wazza-is-awesome')
-      logger.info "Sent email to #{@post.email}"
-      puts "Sent email to #{@post.email}"
+      dbg_str = "Sent email to #{@post.email} with env vars: " +
+        "[domain] #{ENV['NF_SENDGRID_DOMAIN']} [username] #{ENV['NF_SENDGRID_USERNAME']} [password] #{ENV['NF_SENDGRID_PASSWORD']}"
+      logger.info dbg_str
+      puts dbg_str
     else
       logger.info "No email specified for this record. Not sending email."
       puts "No email specified for this record. Not sending email."
