9c28748954b303fd7d85ba2595257e4526856370
Warren Bain <warren@ninefold.com>
2014-08-07 14:15:09 -0700

    Fix config for memcached

--- config/initializers/memcached.rb
+++ config/initializers/memcached.rb
@@ -1,8 +1,8 @@
-if memcached_file = Rails.root.join('config/memcached.yml')
+memcached_file = Rails.root.join('config/memcached.yml')
+if File.exist?(memcached_file)
   memcached_config = YAML.load_file(memcached_file)
   if memcached_config['servers']
     memcached_hosts = memcached_config['servers']
   end
 end
 Rails.application.config.cache_store = :dalli_store, *memcached_hosts
-
