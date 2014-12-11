20a4bfd168e46602a1b048eb89602a159b7cf9c8
Shaun Domingo <shaun@ninefold.com>
2014-12-11 14:59:00 +1100

    A couple of fixes.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -73,7 +73,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     respond_to do |format|
-      if @post.update_attributes(params[:post])
+      if @post.update_attributes(params[:post].permit(:name, :title, :content, :email))
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       else
