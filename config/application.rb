ae965ad63f2c4bca753d7957b1840986df6736ad
Warren Bain <warren@ninefold.com>
2015-01-22 16:25:50 +0000

    Default log formatter attempt #3

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ end
 module DeployTest
   class Application < Rails::Application
 
-    config.log_formatter = ::Logger::Formatter.new
+    config.logger = ::Logger.new
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
